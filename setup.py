6a1d72da24715f0f0da4039448a70960e438b0cd
Nick Pisacane <pisacanen@gmail.com>
2016-07-02 20:44:22 -0700

    package info

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='movielens_importer',
+    version='0.0.1',
+    decription='MovieLens data set importer.',
+    long_description='MovieLens data set importer.',
+    url='https://github.com/nickpisacane/movielens_importer',
+    author='Nick Pisacane',
+    author_email='pisacanen@gmail.com',
+    liscense='MIT'
+)
