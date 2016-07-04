fb577571a9dfb2c16f260b0ac18b81646792ef56
Nick Pisacane <pisacanen@gmail.com>
2016-07-03 17:47:16 -0700

    add scripts

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
+import movielens_importer
 
 setup(
     name='movielens_importer',
-    version='0.0.1',
+    version=movielens_importer.__version__,
     decription='MovieLens data set importer.',
     long_description='MovieLens data set importer.',
     url='https://github.com/nickpisacane/movielens_importer',
     author='Nick Pisacane',
     author_email='pisacanen@gmail.com',
-    liscense='MIT'
+    liscense='MIT',
+    scripts=['bin/movielens-download']
 )
